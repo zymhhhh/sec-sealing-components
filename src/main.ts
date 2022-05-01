dbc111b05f514d63070c46e0dcb53e94875b5655
zymhhhh <2335610294@qq.com>
2022-05-01 16:31:55 +0800

    git commit -m "global register icons"

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import { toLine } from './utils/index';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 
 import ELementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as Icons from '@element-plus/icons-vue'
 
 const app = createApp(App)
+// global register icons loading needs time
+for(let i in Icons) {
+    app.component(`el-icon-${toLine(i)}`,(Icons as any)[i])
+}
 app.use(router).use(ELementPlus)
 app.mount('#app')
